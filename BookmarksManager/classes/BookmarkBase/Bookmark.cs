389cf408961ebc5f231ce52bab0499d9fe15ba67
Charles Daudré-Vignier <daudre.vignier.charles@yandex.com>
2019-08-09 14:44:21 +0200

    update

--- BookmarksManager/classes/BookmarkBase/Bookmark.cs
+++ BookmarksManager/classes/BookmarkBase/Bookmark.cs
@@ -1,31 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
-
-namespace BookmarksManager.BookmarkBase
+﻿namespace BookmarksManager.BookmarkBase
 {
     public class Bookmark
     {
-        [XmlElement("name")]
         public string Name;
-
-        [XmlElement("url")]
         public string Url;
-
-        [XmlElement("ie")]
         public bool IE;
-
-        [XmlElement("chrome")]
         public bool Chrome;
-
-        [XmlElement("firefox")]
         public bool Firefox;
-
         public bool IEExist;
-
         public bool ChromeExist;
-
         public bool FirefoxExist;
     }
 }
